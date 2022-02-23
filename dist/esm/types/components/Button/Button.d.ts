11ed3243babf084dfa5a3fd8d1cc339f93e64258
Yahia-2010 <yahiaberash8@gmail.com>
2022-02-23 23:20:58 +0300

    finishing the 0.0.5 version

--- dist/esm/types/components/Button/Button.d.ts
+++ dist/esm/types/components/Button/Button.d.ts
@@ -1,16 +1,16 @@
 import React from "react";
 import { StylingInterface } from "../../global/interfaces/stylingInterface";
-import { sizes, numberSizes, sizedColor } from "../../global/types/types";
+import { sizes, numberSizes, sizedColor, numberSizedColor } from "../../global/types/types";
 import "./Button.css";
 export interface ButtonProps {
-    label: string;
     styling?: StylingInterface;
-    func?: (params: any) => void;
+    func?: () => void;
     size?: sizes;
     bg?: string;
     textColor?: string;
     shadow?: sizedColor;
     roundness?: numberSizes;
+    border?: numberSizedColor;
 }
 declare const Button: React.FC<ButtonProps>;
 export default Button;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
 interface StylingInterface {
 }
 
 declare type sizes = "sm" | "md" | "lg" | "xl";
 declare type numberSizes = sizes | number;
+declare type borderStyles = "solid" | "dotted" | "dashed" | "double" | "groove" | "ridge" | "inset" | "outset";
 declare type sizedColor = {
     size: sizes;
     color: string;
 };
+declare type numberSizedColor = {
+    size: numberSizes;
+    color: string;
+    style: borderStyles;
+};
 
 interface ButtonProps {
-    label: string;
     styling?: StylingInterface;
-    func?: (params: any) => void;
+    func?: () => void;
     size?: sizes;
     bg?: string;
     textColor?: string;
     shadow?: sizedColor;
     roundness?: numberSizes;
+    border?: numberSizedColor;
 }
 declare const Button: React.FC<ButtonProps>;
 
-export { Button };
+interface IconButtonProps {
+    icon: ReactElement;
+    func?: () => void;
+    styling?: StylingInterface;
+    size?: sizes;
+    bg?: string;
+    shadow?: sizedColor;
+    border?: numberSizedColor;
+    textColor?: string;
+}
+declare const IconButton: React.FC<IconButtonProps>;
+
+export { Button, IconButton };
